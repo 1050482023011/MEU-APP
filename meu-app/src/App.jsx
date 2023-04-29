2c2025711b872a5a2bff47bbfee6e0f32123edca
Thiago <thiago.alves30@fatec.sp.gov.br>
2023-04-28 21:54:50 -0300

    atualizacaoapp

--- meu-app/src/App.jsx
+++ meu-app/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       </p>
       <br />
 
-      <CustonInput type="text"  placeholder="Nome" />
+      <CustonInput type="text"  placeholder="Nome" /> <div className="br"></div>
       <CustonInput type="text"  placeholder="Telefone" /><div className="br"></div>
       <CustonInput type="text"  placeholder="E-mail" /><div className="br"></div> <br />
       <ButtonFatec type="button" label="Enviar" /> <div className="br"></div> 
